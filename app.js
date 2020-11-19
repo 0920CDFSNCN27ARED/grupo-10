7d754d79cf8d07369b532fe65bbedb8299d11b29
danielreyes-7 <dhro7rm@gmail.com>
2020-11-18 23:25:07 -0300

    Terminar de darle estilo

--- app.js
+++ app.js
@@ -1,28 +1,32 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 
-app.use(express.static('public'));
+app.use(express.static("public"));
 
 app.listen(3000, () => {
-    console.log('Servidor funcionando en puerto 3000');
-})
-
-app.all('/', (req, res)=>{
-    res.sendFile(__dirname + '/views/index.html')
+    console.log("Servidor funcionando en puerto 3000");
 });
 
-app.get('/products', (req, res)=>{
-    res.sendFile(__dirname + '/views/products.html')
+app.all("/", (req, res) => {
+    res.sendFile(__dirname + "/views/index.html");
 });
 
-app.get('/cart', (req, res)=>{
-    res.sendFile(__dirname + '/views/productCart.html')
+app.get("/products", (req, res) => {
+    res.sendFile(__dirname + "/views/products.html");
 });
 
-app.get('/register', (req, res)=>{
-    res.sendFile(__dirname + '/views/register.html')
+app.get("/cart", (req, res) => {
+    res.sendFile(__dirname + "/views/productCart.html");
 });
 
-app.get('/login', (req, res)=>{
-    res.sendFile(__dirname + '/views/login.html')
+app.get("/register", (req, res) => {
+    res.sendFile(__dirname + "/views/register.html");
+});
+
+app.get("/login", (req, res) => {
+    res.sendFile(__dirname + "/views/login.html");
+});
+
+app.get("/pre", (req, res) => {
+    res.sendFile(__dirname + "/views/pre-home.html");
 });
