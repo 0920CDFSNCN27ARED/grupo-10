a7cdf513230ee9b3362501d028e3906a40cf7252
danielreyes-7 <dhro7rm@gmail.com>
2020-12-08 20:07:17 -0300

    transicion a ejs

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 const express = require("express");
 const app = express();
 
+app.set("view engine", "ejs");
+app.set("view", __dirname + "views");
+
 app.use(express.static("public"));
 
 app.listen(3000, () => {
