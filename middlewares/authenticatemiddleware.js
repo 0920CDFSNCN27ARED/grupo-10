45033432fd29cd63b5521ef99b43b18cecfed979
danielreyes-7 <dhro7rm@gmail.com>
2021-03-27 22:27:23 -0300

    front validations added

--- middlewares/authenticatemiddleware.js
+++ middlewares/authenticatemiddleware.js
@@ -3,13 +3,15 @@ const db = require("../db/models");
 async function authenticatemiddleware(req, res, next) {
     const id = req.session.loggedUserId;
     console.log(id);
-    if (!id) next();
+
+    if (!id) return next();
 
     const loggedUser = await db.Users.findByPk(id);
     if (!loggedUser) {
         delete req.session.loggedUserId;
         return next();
     }
+
     req.loggedUser = loggedUser;
 
     res.locals.user = loggedUser;
